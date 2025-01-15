f30facda861d75226e2c3d44cfafd43e197261e4
hyobin <hyobin90@gmail.com>
2025-01-14 21:10:07 -0500

    GTPS-006 GTPS-008 completed

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 """ Defines `game` class"""
 from enum import Enum
 from typing import Dict, List
+from datetime import datetime
 
 
 URL_METACRITIC = 'https://www.metacritic.com/game/'
@@ -8,39 +9,63 @@ URL_OPENCRITIC = 'https://opencritic.com/game/'
 
 class Platform(Enum):
     """Collections of platforms"""
-    PS5 = 'PlayStation5'
-    PS5_PRO = 'PlayStation5 Pro'
+    PS4 = 'PlayStation 4'
+    PS5 = 'PlayStation 5'
+
 
 class Status(Enum):
-    """Collectons of current status of playing a game"""
+    """Collectons of current status of a game."""
     ANNOUNCED = 'announced'
     COMMING = 'comming' 
-    COMMING_SOON = 'comming soon' # TODO let the user define how soon
+    COMMING_SOON = 'comming soon' # GTPS-010 Allow the user to define the sense of how soon
     RELEASED = 'released'
+    PURCHASE = 'purchased'
     PLAYING = 'playing'
     PAUSED = 'paused'
     COMPLETED = 'completed as aimed'
-    # TODO Find logics to describe completed but still playing including playing through multiple time
-    # TODO in the client, the options for the status should be provided as a drop-down list
 
-# TODO with PSN API, would it be possible to add an entry automatically each time a game is purchased?
+class Expectation(Enum):
+    """Collection of the level of being excited about the game."""
+    VERY_HIGH = 3
+    HIGH = 2
+    INTERESTED = 1
+    NOTICING = 0
+
+class Goals(Enum):
+    """Collections of frequently mentioned goals for the game."""
+    ALL_TRPOHYIES = 'all_trophies'
+
+
 class Game:
     """A class to hold metadata on a game"""
-    def __init__(self, wikidata: Dict[str, str]):
+    def __init__(self, personal_data: Dict[str, str], wikidata: Dict[str, str]):
         # attributes to be filled by user
-        self.my_score = 0
-        self.goals = {}
-        #self.pro_enhanced: bool = False
-        #self.note
-        #self.released = released # TODO verify compared to the current date with the release date
-        #self.purchase_date = purchase_date
-        #self.play_platform = playing_platform
-        # self.status = status
-        # self.hyped_level = hyped_level
+        ## filled up initally
+        try:
+            self.purchase_date = datetime.strftime(personal_data['purchase_date'], '%Y-%m-%d')
+        except ValueError:
+            raise RuntimeError('The date format is not matched.')
+        if not (personal_data['play_platform'] == str(Platform.PS4.name) or 
+                (personal_data['play_platform'] == str(Platform.PS5.name))):
+            raise RuntimeError(f'Invalid platform was provided : {personal_data["play_platform"]}')
+        else:
+            self.play_platform = Platform(personal_data['play_platform'])
+        if not (0 <= int(personal_data['expectation']) <=3):
+            raise RuntimeError(f'Invalid expectation value was provided : {personal_data["expectation"]}')
+        else:
+            self.expectaion = Expectation(int(personal_data['expectation'])).value
+        
+        # TODO maybe with NoSQL implemented
+        # self.goals = {}
+        # self.note = ''
+        # self.my_score = 0
 
         # attributes to be filled automatically
+        ## updated automatically
+        # self.released = released # TODO verify compared to the current date with the release date
+        # self.status = status
+        
         ## from `WikiData`
-
         self.wikipedia_link = wikidata['wikipedia_link']
         self.wikidata_link = wikidata['wikidata_link']
         self.genres = wikidata['genres']
@@ -57,45 +82,8 @@ class Game:
         self.open_critics_score = 0
         self.open_user_score = 0
 
-
-
-        # self.metadata_sources: List[Dict]= [
-        #     {
-        #         'attribute': 'genre',
-        #         'selector': '',
-        #         'variable': self.genre
-        #     },
-        #     {
-        #         'attribute': 'developer',
-        #         'selector': '',
-        #         'variable': self.developer
-        #     },
-        #     {
-        #         'attribute': 'publisher',
-        #         'selector': '',
-        #         'variable': self.publisher
-        #     },
-        #     {
-        #         'attribute': 'meta_critics_score',
-        #         'selector': '',
-        #         'variable': self.meta_critics_score
-        #     },
-        #     {
-        #         'attribute': 'meta_user_score',
-        #         'selector': '',
-        #         'variable': self.meta_user_score
-        #     },
-        #     {
-        #         'attribute': 'open_critics_score',
-        #         'selector': '',
-        #         'variable': self.open_critics_score
-        #     },
-        #     {
-        #         'attribute': 'open_user_score',
-        #         'selector': '',
-        #         'variable': self.open_user_score
-        #     }
-        # ]
+        # from `PSN API`?
+        #self.pro_enhanced: bool = False
 
 
     def _parse_date(self) -> None:
@@ -107,4 +95,3 @@ class Game:
         """Extract and store desired data by crawling the source URL."""
         pass
 
-    
