54f167ab7c1be2efecdcd475a74acc2363627ebd
hyobin <hyobin90@gmail.com>
2025-01-23 22:20:27 -0500

    GTPS-59 updated db_manager.py with aiomysql to support asyn process

--- db_manager.py
+++ db_manager.py
@@ -1,6 +1,7 @@
+import asyncio
+from aiomysql import connect, OperationalError, ProgrammingError
 from dotenv import load_dotenv
 import os
-from pymysql import connect, OperationalError, ProgrammingError
 
 
 # load the credential for the DB
@@ -12,29 +13,29 @@ local_db_user = os.getenv('LOCAL_SQL_DB_USER')
 local_db_passwd = os.getenv('LOCAL_SQL_PASSWD')
 game_db_schema_path = os.path.join(os.getcwd(), 'DB', 'game_db_schema.sql')
 
-def create_db(host: str, port: int, user: str, passwd: str, db_name: str, schema_path: str):
+async def create_db(host: str, port: int, user: str, passwd: str, db_name: str, schema_path: str):
     """Creates a database from a schema file.
     Args:
         schema_path: the path to .sql file containg the shcema.
     """
     try:
-        db_connection = connect(host=host, user=user, passwd=passwd, database=db_name, port=port)
-        db_cursor = db_connection.cursor()
+        db_connection = await connect(host=host, user=user, password=passwd, db=db_name, port=port)
 
-        with open(schema_path, 'r') as f:
-            schema = f.read()
-            db_cursor.execute(schema)
-        print(f'{db_name} has been created successfully.')
+        async with db_connection.cursor() as db_cursor:
+            with open(schema_path, 'r') as f:
+                schema = f.read()
+                await db_cursor.execute(schema)
+            print(f'{db_name} has been created successfully.')
 
     except OperationalError as e:
         print(f'Error occurred : {e}')
         print(f'Creating a new DB, {db_name}')
-        db_connection = connect(host=host, user=user, passwd=passwd, port=port)
-        db_cursor = db_connection.cursor()
-        db_cursor.execute(f'CREATE DATABASE {db_name}')
-        db_cursor.execute(f'USE {db_name}')
-        create_db(local_db_host, local_db_port, local_db_user, local_db_passwd, 'game_db', game_db_schema_path)
+        db_connection = await connect(host=host, user=user, passwd=passwd, port=port)
+        async with db_connection.cursor() as cursor:
+            await db_cursor.execute(f'CREATE DATABASE {db_name}')
+            await db_cursor.execute(f'USE {db_name}')
+            await create_db(local_db_host, local_db_port, local_db_user, local_db_passwd, 'game_db', game_db_schema_path)
     except ProgrammingError as e:
         print(f'Error occurred : {e}')
 
-create_db(local_db_host, local_db_port, local_db_user, local_db_passwd, 'game_db', game_db_schema_path)
+asyncio.run(create_db(local_db_host, local_db_port, local_db_user, local_db_passwd, 'game_db', game_db_schema_path))
